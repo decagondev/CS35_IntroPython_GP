62a1cad16a17d608fe048175873b2d474716f1df
Tom Tarpey <tomtarpeydev@gmail.com>
2020-08-31 17:07:35 +0100

    day 1 start

---
+++ AGENDA.md
@@ -0,0 +1,11 @@
+# Intro to Python Agenda
+
+## Day 1 (Intro to Python Programming)
+- Info and logistics
+- Python resources
+- 5 min break
+- Text and printing
+- Collections (Lists)
+- 5 min break
+- Collections (Dictionaries)
+- wrap up
---
+++ day1/00_intro.py
@@ -0,0 +1,85 @@
+
+# This is a comment
+
+# lets print a string
+# print("Hello, CS35!", "Some other text", "and theres more...")
+# variables
+# label = value
+# let const var (js)
+# int bool short (c)
+# first_name = "Tom"
+# # print("Hello CS35 and" , first_name)
+# num = 23.87
+
+# # f strings
+# my_string = "    this is a string tom"
+# print(my_string)
+
+# print(my_string.strip())
+# print(len(my_string))
+# print(len(my_string.strip()))
+
+# print(f"Hello CS35 and                {first_name}.......")
+# print("something on a new line")
+
+
+
+
+# collections
+
+# create an empty list? Array
+
+
+# print(my_list)
+
+# create a list with numbers 1, 2, 3, 4, 5
+
+# add an element 24 to lst1
+
+# print(lst1)
+
+# # print all values in lst2
+# print(lst2)
+
+
+# loop over the list using a for loop
+
+
+# while loop
+
+
+# List Comprehensions
+
+# Create a new list containing the squares of all values in 'numbers'
+numbers = [1, 2, 3, 4]
+squares = []
+
+print(numbers)
+print(squares)
+# Filtering with a list comprehension
+evens = []
+
+
+
+# create a new list of even numbers using the values of the numbers list as inputs
+
+print(evens)
+
+# create a new list containing only the names that start with 's' make sure they are capitalized (regardless of their original case)
+names = ["Sarah", "jorge", "sam", "frank", "bob", "sandy"]
+s_names = []
+print(s_names)
+
+
+
+
+
+# Dictionaries
+
+# Create a new dictionary
+
+# empty
+
+# key value pairs
+
+# access an element via its key
