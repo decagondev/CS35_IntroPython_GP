9ecff8ba9b4f8e7441da6e677d73457562db55fb
Tom Tarpey <tomtarpeydev@gmail.com>
2020-09-03 18:36:29 +0100

    day 4 end

--- day4/category.py
+++ day4/category.py
@@ -1,9 +1,9 @@
 # lets create a class to hold our category data
 
 class Category:
-    def __init__(self, name):  #, products):
+    def __init__(self, name, products):
         self.name = name
-        # self.products = products
+        self.products = products
 
     def __str__(self):
         return f"No Products in {self.name}"
---
+++ day4/clothing.py
@@ -0,0 +1,11 @@
+from product import Product
+
+class Clothing(Product):
+    def __init__(self, name, price, color, size, ID, description):
+        super().__init__(name, price)
+
+        self.color = color
+        self.size = size
+
+    def __str__(self):
+        return f"{super().__str__()} comes in {self.color}, {self.size}"
